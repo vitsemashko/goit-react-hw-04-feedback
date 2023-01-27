4e0f0fe4a27774f18d1825c87ae0244ba0957553
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2023-01-27 16:48:59 +0200

    Updated
    1) updated feedback options styles for buttons;
    2) updated styles for statistics component

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,25 +1,29 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import css from "./FeedbackOptions.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import css from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ onClick }) => {
-	const options = ["Good", "Neutral", "Bad"];
-	return (
-		<div className={css.feedbackOptions}>
-			{options.map((option) => {
-				return (
-					<Button key={option} onClick={onClick} variant="primary">
-						{option}
-					</Button>
-				);
-			})}
-		</div>
-	);
+  const options = [
+    { id: '1', title: 'Good', variant: 'primary' },
+    { id: '2', title: 'Neutral', variant: 'secondary' },
+    { id: '3', title: 'Bad', variant: 'danger' },
+  ];
+  return (
+    <div className={css.feedbackOptions}>
+      {options.map(option => {
+        return (
+          <Button key={option.id} onClick={onClick} variant={option.variant}>
+            {option.title}
+          </Button>
+        );
+      })}
+    </div>
+  );
 };
 
 FeedbackOptions.propTypes = {
-	onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default FeedbackOptions;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
-import css from "./Statistics.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positiveFeedback }) => {
-	return (
-		<div>
-			<p className={css.option}>Good:{good}</p>
-			<p className={css.option}>Neutral:{neutral}</p>
-			<p className={css.option}>Bad:{bad}</p>
-			<p className={css.option}>Total:{total}</p>
-			<p className={css.option}>Positive feedback:{positiveFeedback}%</p>
-		</div>
-	);
+  return (
+    <div className={css.option}>
+      <p className={css.good}>Good: {good}</p>
+      <p className={css.neutral}>Neutral: {neutral}</p>
+      <p className={css.bad}>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p className={css.positive}>Positive feedback: {positiveFeedback}%</p>
+    </div>
+  );
 };
 
 Statistics.propTypes = {
-	good: PropTypes.number.isRequired,
-	neutral: PropTypes.number.isRequired,
-	bad: PropTypes.number.isRequired,
-	total: PropTypes.number.isRequired,
-	positiveFeedback: PropTypes.number.isRequired,
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positiveFeedback: PropTypes.number.isRequired,
 };
 
 export default Statistics;
