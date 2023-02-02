be47bc207dcdece38f3bb106d436cdcb77e0e360
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2023-02-02 16:56:08 +0200

    Updated
    1) uninstalled unuseful libraries;
    2) deleted dublicated function`s parameters;
    3) added prop options for FeedbackOptions component;
    4) deleted styles which not used

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,76 @@
-import React from "react";
-import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
-import Notification from "./components/Notification/Notification";
-import Section from "./components/Section/Section";
-import Statistics from "./components/Statistics/Statistics";
+import React from 'react';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Notification from './components/Notification/Notification';
+import Section from './components/Section/Section';
+import Statistics from './components/Statistics/Statistics';
 
 const App = () => {
-	const [good, setGood] = React.useState(0);
-	const [neutral, setNeutral] = React.useState(0);
-	const [bad, setBad] = React.useState(0);
-	const onGoodClick = (e) => {
-		setGood((s) => {
-			return s + 1;
-		});
-	};
-	const onNeutralClick = (e) => {
-		setNeutral((s) => {
-			return s + 1;
-		});
-	};
-	const onBadClick = (e) => {
-		setBad((s) => {
-			return s + 1;
-		});
-	};
-	const onButtonClick = (e) => {
-		switch (e.target.textContent) {
-			case "Good":
-				onGoodClick();
-				break;
-			case "Neutral":
-				onNeutralClick();
-				break;
-			case "Bad":
-				onBadClick();
-				break;
-			default:
-				break;
-		}
-	};
-	const onCountTotalFeedback = (good, neutral, bad) => {
-		return good + neutral + bad;
-	};
-	const countPositiveFeedbackPercentage = (good, neutral, bad) => {
-		return Math.round((good * 100) / (good + neutral + bad));
-	};
-	return (
-		<div>
-			<Section title="Please leave feedback">
-				<FeedbackOptions onClick={onButtonClick} />
-			</Section>
-			<Section title="Statistics">
-				{onCountTotalFeedback(good, neutral, bad) ? (
-					<Statistics
-						good={good}
-						bad={bad}
-						neutral={neutral}
-						total={onCountTotalFeedback(good, neutral, bad)}
-						positiveFeedback={countPositiveFeedbackPercentage(
-							good,
-							neutral,
-							bad
-						)}
-					/>
-				) : (
-					<Notification message="There is no feedback" />
-				)}
-			</Section>
-		</div>
-	);
+  const [good, setGood] = React.useState(0);
+  const [neutral, setNeutral] = React.useState(0);
+  const [bad, setBad] = React.useState(0);
+  const onGoodClick = e => {
+    setGood(s => {
+      return s + 1;
+    });
+  };
+  const onNeutralClick = e => {
+    setNeutral(s => {
+      return s + 1;
+    });
+  };
+  const onBadClick = e => {
+    setBad(s => {
+      return s + 1;
+    });
+  };
+  const onButtonClick = e => {
+    switch (e.target.textContent) {
+      case 'good':
+        onGoodClick();
+        break;
+      case 'neutral':
+        onNeutralClick();
+        break;
+      case 'bad':
+        onBadClick();
+        break;
+      default:
+        break;
+    }
+  };
+  const onCountTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good * 100) / (good + neutral + bad));
+  };
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys({ good, neutral, bad })}
+          onClick={onButtonClick}
+        />
+      </Section>
+      <Section title="Statistics">
+        {onCountTotalFeedback() ? (
+          <Statistics
+            good={good}
+            bad={bad}
+            neutral={neutral}
+            total={onCountTotalFeedback(good, neutral, bad)}
+            positiveFeedback={countPositiveFeedbackPercentage(
+              good,
+              neutral,
+              bad
+            )}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
 };
 
 export default App;
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
 import css from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ onClick }) => {
-  const options = [
-    { id: '1', title: 'Good', variant: 'primary' },
-    { id: '2', title: 'Neutral', variant: 'secondary' },
-    { id: '3', title: 'Bad', variant: 'danger' },
-  ];
+const FeedbackOptions = ({ options, onClick }) => {
   return (
     <div className={css.feedbackOptions}>
       {options.map(option => {
         return (
-          <Button key={option.id} onClick={onClick} variant={option.variant}>
-            {option.title}
-          </Button>
+          <button key={option} className={css.optionBtn} onClick={onClick}>
+            {option}
+          </button>
         );
       })}
     </div>
